2e90adfacdf3c6f53401e19ff4383a6cfd6ef1bd
Tran Tung <trantung04@gmail.com>
2017-08-07 14:54:19 +0700

    Add project files.

---
+++ TryAngular2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TryAngular2.Startup))]
+namespace TryAngular2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
